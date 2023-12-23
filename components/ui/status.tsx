42543be8c71fea3c963ae21d59c33a59bde83115
Ryan Chung <msc2pd@virginia.edu>
2023-12-23 12:31:15 +0100

    status

--- components/ui/status.tsx
+++ components/ui/status.tsx
@@ -3,17 +3,17 @@ const StatusUpdates = () => {
     <div className="flex flex-col justify-start my-4 space-y-4">
       <div className="flex items-center space-x-2 animate-fadeIn opacity-0 delay-500">
         <div className="w-2 h-2 bg-green-500 rounded-full animate-pulse"></div>
-        <span className="font-normal text-text">Currently listening to:</span>
+        <span className="font-normal text-text">Listening to:</span>
         <span className="font-medium">More Baby - Chris Lake</span>
       </div>
       <div className="flex items-center space-x-2 animate-fadeIn opacity-0 delay-600 delay">
         <div className="w-2 h-2 bg-blue-500 rounded-full animate-pulse"></div>
-        <span className="font-normal text-text">Currently working on:</span>
+        <span className="font-normal text-text">Working on:</span>
         <span className="font-medium">Be Around</span>
       </div>
       <div className="flex items-center space-x-2 animate-fadeIn opacity-0 delay-700">
         <div className="w-2 h-2 bg-red-500 rounded-full animate-pulse"></div>
-        <span className="font-normal text-text">Currently taking:</span>
+        <span className="font-normal text-text">Courses:</span>
         <span className="font-medium">CS 3120, CS 3240</span>
       </div>
     </div>
